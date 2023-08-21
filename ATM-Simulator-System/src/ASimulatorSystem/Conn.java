045a3164d7be9d6f3252d03efdf9a96828b66b7f
shivamverma26 <94590743+shivamverma26@users.noreply.github.com>
2023-08-21 10:50:27 +0530

    ATM Simulator Application

---
+++ ATM-Simulator-System/src/ASimulatorSystem/Conn.java
@@ -0,0 +1,20 @@
+package ASimulatorSystem;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","paradox61");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
